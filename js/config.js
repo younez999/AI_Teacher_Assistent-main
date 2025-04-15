70400c0b0aba31d19c7b12c079c85e9626fb429e
younez999 <lazreqyounes45@gmail.com>
2025-04-15 21:08:47 +0800

    hide keys (2)

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const API_KEY = 'your-openrouter-api-key';
 const API_URL = "https://openrouter.ai/api/v1/chat/completions";
 const MODEL = "deepseek/deepseek-r1-distill-llama-70b:free";
-// yt api key 
+// youtube api key 
 const apiKey = 'your-youtube-api-key';
